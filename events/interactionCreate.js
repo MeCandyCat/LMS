c8ddb1d5c06b0f9ecc2d94b77214245fce82185b
MihinMUD <mihinudissanayake@gmail.com>
2023-07-21 17:07:37 +0530

    Removed self-register

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,31 +1,27 @@
 const { Events } = require("discord.js");
 
 let doesHavePerm = async (interaction, command, Mongo) => {
+    let hasPerm = true;
     try {
         let perms;
-        if (command.perms) {
+        if (command?.perms) {
             if (Array.isArray(command.perms)) {
                 perms = command.perms;
-            } else if (Array.isArray(command.perms[interaction.options.getSubcommand()])) {
+            } else {
                 perms = command.perms[interaction.options.getSubcommand()];
             }
         }
         if (perms) {
-            const hasPerm = await Mongo.hasPerm(interaction, perms).catch((e) => {
-                // 2 means roles are not configured propperly
-                return 2;
+            hasPerm = await Mongo.hasPerm(interaction, perms).catch((e) => {
+                return "notConfigured";
             });
-            if (!hasPerm) {
-                // 0 means no permission
-                return 0;
-            }
         }
-
-        return 1;
     } catch (e) {
         console.log(e);
-        return 4;
+        hasPerm = "error";
     }
+
+    return hasPerm;
 };
 
 module.exports = {
@@ -35,20 +31,21 @@ module.exports = {
             const command = interaction.client.commands.get(interaction.commandName);
             let hasPerms = await doesHavePerm(interaction, command, Mongo);
             switch (hasPerms) {
-                case 0:
+                case false:
                     await interaction.reply({
                         content: "You don't have permission to run this command.",
                         ephemeral: true,
                     });
                     return;
-                case 1:
+                case true:
                     await command.execute(interaction, Mongo);
                     return;
-                case 2:
+                case "notConfigured":
                     await interaction.reply({ content: "Roles are not configured properly.", ephemeral: true });
                     return;
+                case "error":
+                    await interaction.reply("Something went wrong.");
             }
-            await interaction.reply("Something went wrong.");
         }
         if (interaction.isButton()) {
             if (interaction.customId == "assignmentDel") {
